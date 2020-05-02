91a29a4bba4ca1e3a296c47719e82619116697d3
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-05-02 11:33:56 -0400

    ready to deploy

--- api/server.js
+++ api/server.js
@@ -12,6 +12,9 @@ server.use(helmet());
 server.use(cors());
 server.use(express.json());
 
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'running' });
+  });
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
